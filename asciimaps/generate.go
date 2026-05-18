8dfa8f761dbb8c78e277d2f9227f8f728699d178
dainyamz <terkimbiterungwa@gmail.com>
2026-05-18 04:05:44 +0100

    Generate

--- asciimaps/generate.go
+++ asciimaps/generate.go
@@ -1,5 +1,16 @@
 package ascii
 
-func GenerateBanner(str string, banner map[rune][]string) string {
+import "strings"
 
+func GenerateBanner(str string, banner map[rune][]string) string {
+	sstr := SplitInput(str)
+	var words strings.Builder
+
+	for _, cha := range sstr {
+		line := RenderLine(cha, banner)
+		for _, ch := range line {
+			words.WriteString(ch + "\n")
+		}
+	}
+	return words.String()
 }
